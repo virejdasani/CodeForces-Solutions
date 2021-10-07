14568da51fd000580863a20609fb07f3190bdc74
reganlaurell <reganlaurell@gmail.com>
2021-10-06 22:10:52 -0400

    Code Force CSolution - Caps in Kotlin

---
+++ WordCapitalization-281A/caps.kt
@@ -0,0 +1,33 @@
+// https://codeforces.com/problemset/problem/281/A
+
+// Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+// Note, that during capitalization all the letters except the first one remains unchanged.
+
+// Input
+// A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+// Output
+// Output the given word after capitalization.
+
+// Examples
+// inputCopy
+// ApPLe
+// outputCopy
+// ApPLe
+// inputCopy
+// konjac
+// outputCopy
+// Konjac
+
+import java.util.Scanner
+
+fun main() {
+    print("Enter a word: ")
+    val scanner = Scanner(System.`in`)
+    val strInput = scanner.nextLine()
+    scanner.close()
+
+    print(strInput.replaceFirstChar { it.uppercase() })
+}
+
