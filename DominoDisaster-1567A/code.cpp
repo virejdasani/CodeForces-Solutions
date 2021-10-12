98308d83448db847a5bfd4d07f872f65a2948059
Om <w3docs_email@example.com>
2021-10-12 19:28:15 +0530

    Added solution for 1567A

---
+++ DominoDisaster-1567A/code.cpp
@@ -0,0 +1,38 @@
+// https://codeforces.com/contest/1567/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string k="";
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='U')
+            {
+                k+='D';
+            }
+            else if(s[i]=='D')
+            {
+                k+='U';
+            }
+            else if(s[i]=='L'||s[i]=='R')
+            {
+                k+='L';
+                k+='R';
+                i++;
+            }
+        }
+        cout<<k<<endl;
+    }
+    return 0;
+    
+}
