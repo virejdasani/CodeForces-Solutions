c140c7ea064ac6092cc8695671bbb2eb855a5e8e
Om <w3docs_email@example.com>
2021-10-13 00:11:13 +0530

    Added solution for 1560B

---
+++ WhosOpposite?-1560B/code.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/contest/1560/problem/B
+
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b)
+    {
+        cout<<-1<<endl;
+        continue;
+    }
+    int mn=abs(a-b);
+    if(a>2*mn||b>2*mn||c>2*mn)
+    {
+        cout<<-1<<endl;
+        continue;
+    }
+    if(c+mn<=2*mn)
+        cout<<c+mn<<endl;
+    else if(c-mn>0)
+        cout<<c-mn<<endl;
+    else
+        cout<<-1<<endl;
+    }
+    return 0;
+}
