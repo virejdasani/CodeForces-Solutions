808f9c42e8062eb403bd2d584bae0dcf7df946b6
Jayati Dixit <77896511+Jayati15@users.noreply.github.com>
2021-10-14 14:56:25 +0530

    Create solution.cpp

---
+++ ConsecutiveSumRiddle-1594A/solution.cpp
@@ -0,0 +1,20 @@
+/*
+https://codeforces.com/problemset/problem/1594/A
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+	// your code goes here
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	  ll n;
+	  cin>>n;
+	  
+	  cout<<-1*(n-1)<<" "<<n<<endl;
+	}
+	return 0;
+}
