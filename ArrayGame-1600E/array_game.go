e86db0c402c64844eed896cc793c4fa0e7dc5580
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-10-22 15:31:01 +0530

    little code changes and working code

--- ArrayGame-1600E/array_game.go
+++ ArrayGame-1600E/array_game.go
@@ -22,8 +22,6 @@ func main() {
 		inputArray = append(inputArray, element)
 	}
 
-	count := lengthOfArray
-
 	// participants list and if the
 	participants := []string{"Alice", "Bob"}
 
@@ -32,61 +30,54 @@ func main() {
 	start, end := 0, lengthOfArray-1
 
 gameLoop:
-	for i := start; i < end; i++ {
+	for i := start; i < lengthOfArray; i++ {
 		k := 0
 		for _, val := range participants {
-			if count == 0 {
-				winner = currentPlayer
-				break gameLoop
-			}
 			currentPlayer = val
 
 		selectSideToRemove:
 			fmt.Println("which number you want to remove : left OR right")
 			fmt.Scanln(&choice)
 			switch choice {
+
 			case "left":
 				if len(newSequence) == 0 {
 					newSequence = append(newSequence, inputArray[start])
-
 				} else {
 					if newSequence[k] < inputArray[start] {
 						newSequence = append(newSequence, inputArray[start])
 					}
 					k = k + 1
-					start = start + 1
 				}
-
+				start = start + 1
 			case "right":
 				if len(newSequence) == 0 {
 					newSequence = append(newSequence, inputArray[end])
-
 				} else {
 					if newSequence[k] < inputArray[end] {
 						newSequence = append(newSequence, inputArray[end])
 					}
 					k = k + 1
-					end = end - 1
 				}
-
+				end = end - 1
 			default:
 				goto selectSideToRemove
 			}
 
-			// checking if all the elements are used then returning the winner
-			if len(newSequence) == lengthOfArray {
-				winner = val
-				break gameLoop
-			}
 			lengthNewSeq := len(newSequence)
 
+			// checking if all the elements are used then returning the winner
+			if lengthNewSeq == lengthOfArray {
+				winner = currentPlayer
+				break gameLoop
+			}
+
+			fmt.Println(newSequence[lengthNewSeq-1], inputArray[start], inputArray[end])
 			//checking that last element of the new array is greater than both the side ending element
 			// return the winner in that case
 			if newSequence[lengthNewSeq-1] >= inputArray[start] && newSequence[lengthNewSeq-1] >= inputArray[end] {
 				winner = currentPlayer
-				count = 0
-			} else {
-				count = count - 1
+				break gameLoop
 			}
 
 		}
