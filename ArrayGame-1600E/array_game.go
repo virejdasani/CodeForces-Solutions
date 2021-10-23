59d556cb75fe6cbb9ac63efdc130310d33361229
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-10-23 12:04:07 +0530

    time complexity issue resolved

--- ArrayGame-1600E/array_game.go
+++ ArrayGame-1600E/array_game.go
@@ -2,49 +2,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	var lengthOfArray, element int
 	participants := []string{"Alice", "Bob"}
 	inputArray := make([]int, 0)
-	k := make(chan int, 1)
-
-	fmt.Scanln(&lengthOfArray)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	lengthOfArray, _ = strconv.Atoi(scanner.Text())
 	for i := 0; i < lengthOfArray; i++ {
-		fmt.Scan(&element)
+		scanner.Scan()
+		element, _ = strconv.Atoi(scanner.Text())
 		inputArray = append(inputArray, element)
 	}
-	go calcRightTurn(inputArray, k)
-	go calcLeftTurn(inputArray, k)
-	select {
-	case x := <-k:
-		if x%2 == 0 {
-			fmt.Println(participants[0])
-			return
-		}
-		fmt.Println(participants[1])
+
+	k1 := calcRightTurn(inputArray)
+	k2 := calcLeftTurn(inputArray)
+
+	if k1%2 == 0 || k2%2 == 0 {
+		fmt.Println(participants[0])
 		return
 	}
+	fmt.Println(participants[1])
+	return
+
 }
-func calcRightTurn(inputArray []int, k chan int) {
+func calcRightTurn(inputArray []int) (turn int) {
 	start, end := 0, len(inputArray)-1
-	turn := 0
+	turn = 0
 	for start < end && inputArray[end] < inputArray[end-1] {
 		turn = turn + 1
 		end = end - 1
 	}
-	k <- turn
 	return
 }
-func calcLeftTurn(inputArray []int, k chan int) {
+func calcLeftTurn(inputArray []int) (turn int) {
 	start, end := 0, len(inputArray)-1
-	turn := 0
+	turn = 0
 	for start < end && inputArray[start] < inputArray[start+1] {
 		turn = turn + 1
 		end = end - 1
 	}
-	k <- turn
 	return
 }
