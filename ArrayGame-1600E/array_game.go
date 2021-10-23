a56f3ac0537550965bdd931144f4abf9561bcfc2
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-10-23 11:35:34 +0530

    made code changes to remove time complexity

--- ArrayGame-1600E/array_game.go
+++ ArrayGame-1600E/array_game.go
@@ -3,63 +3,48 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func main() {
 	var lengthOfArray, element int
-	var winner string
-	start, end := 0, lengthOfArray-1
-	// participants list and if the
 	participants := []string{"Alice", "Bob"}
 	inputArray := make([]int, 0)
-	newSequence := make([]int, 0)
+	k := make(chan int, 1)
 
-	// taking the input array's length
 	fmt.Scanln(&lengthOfArray)
-	// taking the input array
 	for i := 0; i < lengthOfArray; i++ {
 		fmt.Scan(&element)
 		inputArray = append(inputArray, element)
 	}
-
-gameLoop:
-	for i := start; i < lengthOfArray; i++ {
-		k := 0
-		if inputArray[start] > inputArray[end] {
-			if len(newSequence) == 0 {
-				newSequence = append(newSequence, inputArray[start])
-			} else {
-				if newSequence[k] < inputArray[start] {
-					newSequence = append(newSequence, inputArray[start])
-				}
-				k = k + 1
-			}
-			start = start + 1
-		} else {
-			if len(newSequence) == 0 {
-				newSequence = append(newSequence, inputArray[end])
-			} else {
-				if newSequence[k] < inputArray[end] {
-					newSequence = append(newSequence, inputArray[end])
-				}
-				k = k + 1
-			}
-			end = end - 1
+	go calcRightTurn(inputArray, k)
+	go calcLeftTurn(inputArray, k)
+	select {
+	case x := <-k:
+		if x%2 == 0 {
+			fmt.Println(participants[0])
+			return
 		}
-		lengthNewSeq := len(newSequence)
-		// checking if all the elements are used then returning the winner
-		if lengthNewSeq == lengthOfArray {
-			winner = participants[i%2]
-			break gameLoop
-		}
-
-		//checking that last element of the new array is greater than both the side ending element
-		// return the winner in that case
-		if newSequence[lengthNewSeq-1] >= inputArray[start] && newSequence[lengthNewSeq-1] >= inputArray[end] {
-			winner = participants[i%2]
-			break gameLoop
-		}
+		fmt.Println(participants[1])
+		return
 	}
-	log.Println(winner)
+}
+func calcRightTurn(inputArray []int, k chan int) {
+	start, end := 0, len(inputArray)-1
+	turn := 0
+	for start < end && inputArray[end] < inputArray[end-1] {
+		turn = turn + 1
+		end = end - 1
+	}
+	k <- turn
+	return
+}
+func calcLeftTurn(inputArray []int, k chan int) {
+	start, end := 0, len(inputArray)-1
+	turn := 0
+	for start < end && inputArray[start] < inputArray[start+1] {
+		turn = turn + 1
+		end = end - 1
+	}
+	k <- turn
+	return
 }
