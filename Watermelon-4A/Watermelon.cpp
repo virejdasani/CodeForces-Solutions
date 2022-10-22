3d5bd1d2a263c4e5dce997834c71e6fac20597ed
SamyakVerma <73108355+SamyakVerma@users.noreply.github.com>
2022-10-22 20:01:25 +0530

    Create Watermelon.cpp

---
+++ Watermelon-4A/Watermelon.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/4/A
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int w;
+cin>>w;
+if (w%2==0 && w>2)
+cout<<"YES";
+else 
+cout<<"NO";
+return 0;
+
+
+}
