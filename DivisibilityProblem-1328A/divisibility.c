e9623888c64c35aeeadbb8d88f90fefa01cc9ea8
Amey Karan <44658273+ameykaran@users.noreply.github.com>
2023-10-23 16:04:21 +0530

    Divisibility Problem 1328A

---
+++ DivisibilityProblem-1328A/divisibility.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a % b == 0 ? 0 : b - a % b);
+    }
+
+    return 0;
+}
